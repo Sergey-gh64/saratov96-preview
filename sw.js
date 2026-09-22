f91626386bcc109c33be2c2696bea3b5bdb2072b
GameDev Worker <gamedev-worker@users.noreply.github.com>
2026-09-22 09:11:40 +0400

    Publish Saratov 96 7789412caa28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='s96-runtime-v2';
+const CACHE='s96-runtime-v3';
 const CORE=['./','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
